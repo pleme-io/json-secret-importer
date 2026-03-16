5fb34738d97014f4373aa1a092f7a43f6ad71bd6
drzzln <drzzln@protonmail.com>
2026-03-15 21:09:16 -0400

    refactor: use substrate repo-flake.nix universal builder
    Replaces bespoke flake.nix boilerplate with declarative config via
    substrate's repo-flake.nix. All build logic, devShell tooling, and
    meta attributes derived from (language, builder) dispatch.

--- flake.nix
+++ flake.nix
@@ -10,22 +10,15 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, substrate, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs { inherit system; };
-      mkGoTool = (import "${substrate}/lib/go-tool.nix").mkGoTool;
-    in {
-      packages.default = mkGoTool pkgs {
-        pname = "json-secret-importer";
-        version = "0.0.0-dev";
-        src = self;
-        vendorHash = "sha256-3CZyy7yQk4c8Q5FICNWJpR3/DyN8UVzI1uGFXfsCXcI=";
-        description = "CLI tool to import JSON secrets from a directory into Akeyless via API";
-        homepage = "https://github.com/pleme-io/json-secret-importer";
-      };
-
-      devShells.default = pkgs.mkShellNoCC {
-        packages = with pkgs; [ go gopls gotools ];
-      };
-    });
+  outputs = inputs: (import "${inputs.substrate}/lib/repo-flake.nix" {
+    inherit (inputs) nixpkgs flake-utils;
+  }) {
+    self = inputs.self;
+    language = "go";
+    builder = "tool";
+    pname = "json-secret-importer";
+    vendorHash = "sha256-3CZyy7yQk4c8Q5FICNWJpR3/DyN8UVzI1uGFXfsCXcI=";
+    description = "CLI tool to import JSON secrets from a directory into Akeyless via API";
+    homepage = "https://github.com/pleme-io/json-secret-importer";
+  };
 }
